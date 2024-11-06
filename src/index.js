a8e02706e0a82e0c66fd1d101e07a43567a49a36
AlondraHackerBae <khrissy921@gmail.com>
2024-11-05 21:14:22 -0600

    added basic JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "Love is Love",
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
 
+let poemElement = document.querySelector("#poem-generator");
+poemElement.addEventListener("submit", generatePoem);
